9d7bb2b29a66c78451119a9e7a0ba4818220e48f
AiConnect <aiconnectserbia@gmail.com>
2026-01-10 14:28:51 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -160,7 +160,7 @@ export default function HeroSection() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.5 }}
-          className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
+          className="absolute bottom-4 sm:bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
         >
           <span className="text-white/40 text-xs tracking-widest uppercase">Skrolaj</span>
           <motion.div
